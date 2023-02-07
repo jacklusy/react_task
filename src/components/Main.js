fcf3e90a12c89b1b31509ac32e237cb7d41a96a3
jacklusy <jackalloussi23@gmail.com>
2023-02-07 16:44:34 +0300

    task

---
+++ src/components/Main.js
@@ -0,0 +1,13 @@
+import React from "react"; 
+
+class Main extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello its me jackHello its me jackHello its me jackHello its me jackHello its me jackHello its me jackHello its me jackHello its me jackHello its me jackHello its me jackHello its me jackHello its me jackHello its me jackHello its me jackHello its me jackHello its me jackHello its me jackHello its me jackHello its me jackHello its me jackHello its me jackHello its me jackHello its me jackHello its me jackHello its me jackHello its me jackHello its me jackHello its me jackHello its me jackHello its me jackHello its me jackHello its me jackHello its me jackHello its me jackHello its me jackHello its me jackHello its me jackHello its me jackHello its me jackHello its me jackHello its me jackHello its me jackHello its me jackHello its me jackHello its me jackHello its me jackHello its me jackHello its me jackHello its me jackHello its me jackHello its me jackHello its me jackHello its me jackHello its me jackHello its me jackHello its me jackHello its me jackHello its me jackHello its me jackHello its me jackHello its me jackHello its me jackHello its me jackHello its me jackHello its me jackHello its me jack</h1>
+            </div>
+        )
+    }
+}
+
+export default Main
